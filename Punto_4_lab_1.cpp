60e2b53cde4e31a0b3bdf465e9b4d85ddd43c994
GerardoASP <gerardo.sanchezp@autonoma.edu.co>
2023-01-30 16:43:48 -0500

    Primera version ejercicio 4 del lab 1

---
+++ Punto_4_lab_1.cpp
@@ -0,0 +1,71 @@
+// C++ code
+//Declaración de Variables
+int pulsador = 2;
+int valor_pulsador = 0;
+int contador = 0;
+int numeros[10] = {0,1,2,3,4,5,6,7,8,9};
+int pines_salida[7]={4,5,6,7,8,9,10};
+
+void seleccion_numero(int valor){
+  if(valor == 0){
+    Serial.println("cero");
+    Serial.println("\n");
+  }else if(valor == 1){
+    Serial.println("uno");
+    Serial.println("\n");
+  }else if(valor == 2){
+    Serial.println("dos");
+    Serial.println("\n");
+  }else if(valor == 3){
+    Serial.println("tres");
+    Serial.println("\n");
+  }else if(valor == 4){
+    Serial.println("cuatro");
+    Serial.println("\n");
+  }else if(valor == 5){
+    Serial.println("cinco");
+    Serial.println("\n");
+  }else if(valor == 6){
+    Serial.println("seis");
+    Serial.println("\n");
+  }else if(valor == 7){
+    Serial.println("siete");
+    Serial.println("\n");
+  }else if(valor == 8){
+    Serial.println("ocho");
+    Serial.println("\n");
+  }else if(valor == 9){
+    Serial.println("nueve");
+    Serial.println("\n");
+  }else{
+    Serial.println("cero especial");
+    Serial.println("\n");
+  }
+}
+
+void aumentar(int pul){
+  if(pul == 0){
+    Serial.println("El valor del contador es: ");
+  	Serial.println(contador);
+    contador = contador + 1;
+    delay(200);
+  }else{
+    delay(200);
+  }
+}
+void setup()
+{
+  for(int i=0;i<7;i++){
+    pinMode(pines_salida[i], OUTPUT);
+  }
+  pinMode(pulsador, INPUT);
+  Serial.begin(9600);
+  Serial.println("El valor del contador es: ");
+  Serial.println(contador);
+}
+
+void loop()
+{
+  valor_pulsador = digitalRead(pulsador);
+  aumentar(valor_pulsador);
+}
